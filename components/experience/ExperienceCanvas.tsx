f62af76142b34a03ec2cdc72f740a2dc62f12c69
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2021-10-24 23:23:27 +0200

    fixed loading screen

--- components/experience/ExperienceCanvas.tsx
+++ components/experience/ExperienceCanvas.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import React, { createContext, Suspense, useContext, useState } from "react";
+import React, { createContext, Suspense, useContext, useEffect, useState } from "react";
 import ExperienceCanvasContent from "./ExperienceCanvasContent";
 import { useDebug } from "./ExperienceProvider";
 import LoadingOverlay from "./LoadingOverlay";
@@ -7,16 +7,40 @@ import LoadingOverlay from "./LoadingOverlay";
 const BridgeContext = createContext(false);
 export const useExperienceDebug = () => useContext(BridgeContext);
 
+const CanvasFallback = ({ onFinish }) => {
+  useEffect(() => {
+    return () => {
+      onFinish();
+    }
+  }, [])
+
+  return (
+    <></>
+  );
+}
+
 const ExperienceCanvas = () => {
   const debug = useDebug();
   const [experienceLoaded, setExperienceLoaded] = useState(false);
+  const [isFadingOut, setIsFadingOut] = useState(false);
 
-  const onLoadExperience = () => setExperienceLoaded(true);
+  const onLoadExperience = () => {
+    setTimeout(() => {
+      setIsFadingOut(true);
+      setTimeout(() => setExperienceLoaded(true), 1000);
+    }, 2000);
+  }
 
   return (
     <div className={`fixed inset-0 w-screen h-[calc(100vh - calc(100vh - 100%))] ${experienceLoaded ? '' : 'z-50'}`}>
+      {
+        !experienceLoaded &&
+        <div className={'transition-all duration-1000 ' + (isFadingOut ? 'opacity-0' : 'opacity-100')}>
+          <LoadingOverlay />
+        </div>
+      }
       <Canvas>
-        <Suspense fallback={<LoadingOverlay onFinish={onLoadExperience} />}>
+        <Suspense fallback={<CanvasFallback onFinish={onLoadExperience} />}>
           <BridgeContext.Provider value={debug}>
             <ExperienceCanvasContent />
           </BridgeContext.Provider>
--- components/experience/LoadingOverlay.tsx
+++ components/experience/LoadingOverlay.tsx
@@ -1,29 +1,18 @@
-import { Html, useProgress } from '@react-three/drei';
-import React, { useEffect } from 'react';
+import { useProgress } from '@react-three/drei';
+import React, { useEffect, useState } from 'react';
 
-type Props = {
-  onFinish: () => void
-}
-
-const LoadingOverlay = ({ onFinish }: Props) => {
+const LoadingOverlay = () => {
   const { progress } = useProgress();
+  const [windowWidth, setWindowWidth] = useState(0);
 
   useEffect(() => {
-    return () => {
-      onFinish();
-    }
+    setWindowWidth(window.innerWidth);
   }, [])
 
-  useEffect(() => {
-    console.log(progress);
-  }, [progress])
-
   return (
-    <Html center>
-      <div className="w-screen h-screen bg-black flex justify-center items-center">
-        <div className={`border-2 border-white`} style={{ height: '50px', width: progress + 'px', animation: '1s all' }}></div>
-      </div>
-    </Html>
+    <div className="w-screen h-screen bg-black flex justify-start items-center">
+      <div className={`border-2 border-white`} style={{ width: `${windowWidth}px`, transform: `scaleX(${progress / 100})`, transformOrigin: 'left center', transition: '2s all ease-in-out' }}></div>
+    </div>
   )
 }
 
